c1114ae1d49b6119b8b5a03b36fb0b2e52e03405
Микола Жигун <125123753+MykolaZhigun@users.noreply.github.com>
2023-10-25 11:21:34 +0300

    Add files via upload

---
+++ Laboratory5_task6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+    double number; 
+
+    std::cout << "Enter the number: ";
+    std::cin >> number;
+
+    float floatNumber = static_cast<float>(number);
+    int intNumber = number;
+
+    std::cout << "Float: " << floatNumber << std::endl;
+    std::cout << "Int: " << intNumber << std::endl;
+
+    return 0;
+}
